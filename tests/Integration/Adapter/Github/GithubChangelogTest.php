e1ff24ef0381670df6cf362e03ff66dbb28e2d6c
Daniel Leech <dan.t.leech@gmail.com>
2018-12-10 19:35:43 +0000

    do not run GH API test on Travis

--- tests/Integration/Adapter/Github/GithubChangelogTest.php
+++ tests/Integration/Adapter/Github/GithubChangelogTest.php
@@ -11,6 +11,11 @@ class GithubChangelogTest extends TestCase
 {
     public function testChangelog()
     {
+        if (getenv('TRAVIS')) {
+            $this->markTestIncomplete();
+            return;
+        }
+
         $history = new PackageHistory('phpactor/phpactor', 'git', 'https://github.com/phpactor/phpactor.git');
         $history->addReference('ccce1891f76c28faa4b0f10b67b5b7ed52eb91ad');
         $history->addReference('f5da23415533deed92fe3522da7099dccccf0ff0');
